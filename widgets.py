43e83dea465d7cde766524e28267dbc04778e572
Anurag Kumar <anuragkumar02112002@gmail.com>
2025-11-01 21:24:32 +0530

    Add files via upload

---
+++ widgets.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.title("Streamlit Text Input")
+name=st.text_input("Enter your name:")
+
+age=st.slider("Select your age",0,100,25)
+st.write(f"Your age is {age}.")
+
+options=["Python","Java","C++","JavaScript"]
+choice=st.selectbox("Choose your favorite language:", options)
+st.write(f"You selected {choice}")
+if name:
+    st.write(f"Hello,{name}")
+
+
+data={
+    "Name":["John","Jane","Jake","Jill"],
+    "Age":[28,24,35,40],
+    "City":["New York","Los Angeles","Chicago","Houston"]
+}
+df=pd.DataFrame(data)
+df.to_csv("Sample.csv")
+st.write(df)
+
+uploaded_files=st.file_uploader("Choose a CSV file",type="csv")
+if uploaded_files is not None:
+    df=pd.read_csv(uploaded_files)
+    st.write(df)
